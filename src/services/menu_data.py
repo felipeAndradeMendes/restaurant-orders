b5dc061814e53790b34b0684d12316fb6c248438
Felipe Mendes <felipemadruga@gmail.com>
2023-09-01 19:04:37 -0300

    Conclui requisito 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -16,24 +16,27 @@ class MenuData:
         self.dishes = set()
         self.__dish_names = {}
 
-        with open(source_path) as file:
+        print('SOURCE PATH:', source_path)
+        with open('../' + source_path, "r") as file:
             menu_database = csv.DictReader(file, delimiter=",")
-            header, *data = menu_database
+            data = menu_database
+            # como retornar todas as linhas, sem pular a primeira?
+            # o for estava fora do with open...
 
-        for line in data:
-            name = line["dish"]
-            price = float(line["price"])
-            ingredient = line["ingredient"]
-            amount = line["recipe_amount"]
-            # print('INGREDIENT', ingredient)
-            # print('AMOUNT:', amount)
+            for line in data:
+                name = line["dish"]
+                price = float(line["price"])
+                ingredient = line["ingredient"]
+                amount = int(line["recipe_amount"])
+                # print('INGREDIENT', ingredient)
+                # print('AMOUNT:', amount)
 
-            if name not in self.__dish_names:
-                self.__dish_names[name] = Dish(name, price)
-            self.__dish_names[name].add_ingredient_dependency(
-                Ingredient(ingredient), amount
-            )
-            self.dishes.add(self.__dish_names[name])
+                if name not in self.__dish_names:
+                    self.__dish_names[name] = Dish(name, price)
+                self.__dish_names[name].add_ingredient_dependency(
+                    Ingredient(ingredient), amount
+                )
+                self.dishes.add(self.__dish_names[name])
 
 
 # print(get_dishes_from_csv("../../data/menu_base_data.csv"))
